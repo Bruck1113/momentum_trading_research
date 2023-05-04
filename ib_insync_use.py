add3f513e24af5702ec3f0bee8aadab1e0728fbd
Bruck1113 <78420268+Bruck1113@users.noreply.github.com>
2023-05-04 12:30:14 +0800

    Regular update
    Code that can extract quotes from interactive broker ib_insync api

---
+++ ib_insync_use.py
@@ -0,0 +1,14 @@
+from ib_insync import *
+# util.startLoop()  # uncomment this line when in a notebook
+
+ib = IB()
+ib.connect('127.0.0.1', 7497, clientId=3)
+
+contract = Forex('EURUSD')
+bars = ib.reqHistoricalData(
+    contract, endDateTime='', durationStr='30 D',
+    barSizeSetting='1 hour', whatToShow='MIDPOINT', useRTH=True)
+
+# convert to pandas dataframe:
+df = util.df(bars)
+print(df)
